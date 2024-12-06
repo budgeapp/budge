b391b2d6fd21dc57839a938514493f0f5527a2a1
Jesse Brooklyn Hannah <jesse@jbhannah.net>
2024-12-06 14:36:59 -0800

    RecurringTransaction dataclass

--- src/budge/__init__.py
+++ src/budge/__init__.py
@@ -2,6 +2,7 @@ from dataclasses import dataclass
 from datetime import date
 from typing import Self
 
+from dateutil.rrule import rrule
 from stockholm import Money
 
 
@@ -13,3 +14,14 @@ class Transaction:
 
     def __lt__(self, other: Self):
         return self.date < other.date
+
+
+@dataclass
+class RecurringTransaction:
+    rrule: rrule
+    amount: Money
+    description: str
+
+    def __iter__(self):
+        for next in self.rrule:
+            yield Transaction(next.date(), self.amount, self.description)

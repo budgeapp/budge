394af02e5370bb866768f66333d08bbfa9e616d5
Jesse Brooklyn Hannah <jesse@jbhannah.net>
2024-12-06 13:40:08 -0800

    Transaction dataclass

--- src/budge/__init__.py
+++ src/budge/__init__.py
@@ -1,2 +1,15 @@
-def hello() -> str:
-    return "Hello from budge!"
+from dataclasses import dataclass
+from datetime import date
+from typing import Self
+
+from stockholm import Money
+
+
+@dataclass
+class Transaction:
+    date: date
+    amount: Money
+    description: str
+
+    def __lt__(self, other: Self):
+        return self.date < other.date

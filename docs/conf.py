1b7ab949973e8700132ecb369ae477d837dfadda
Jesse Brooklyn Hannah <jesse@jbhannah.net>
2024-12-08 13:57:55 -0800

    Fix Sphinx system path

--- docs/conf.py
+++ docs/conf.py
@@ -14,7 +14,7 @@ copyright = "2024, Jesse Brooklyn Hannah"
 author = "Jesse Brooklyn Hannah"
 release = "0.2.0"
 
-sys.path.insert(0, os.path.abspath(".."))
+sys.path.insert(0, os.path.abspath("../src"))
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration

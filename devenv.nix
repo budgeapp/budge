aa86de57a820a226fe22ad79f10c946cfd2930f4
Jesse Brooklyn Hannah <jesse@jbhannah.net>
2025-08-28 09:21:05 -0700

    add devenv

---
+++ devenv.nix
@@ -0,0 +1,13 @@
+{ pkgs, ... }:
+{
+  packages = with pkgs; [
+    git
+  ];
+
+  languages.python = {
+    enable = true;
+    uv.enable = true;
+    uv.sync.enable = true;
+    venv.enable = true;
+  };
+}
